c37075515338034476aad4965733fc0b7aef9510
matlesz <matlesz@gmail.com>
2024-07-31 21:03:55 +0100

    installation of tailwind css

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home</h1>
+      <h1 className='text-red-400'>Home</h1>
     </main>
   );
 }
