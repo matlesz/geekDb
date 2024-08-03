702cb883260569301ea7012219162d5ec20c7267
matlesz <matlesz@gmail.com>
2024-08-03 21:15:11 +0100

    feat: Add About page component

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl font-medium text-gray-400">About</h1>
+        <p>
+        Welcome to geeDB, your ultimate destination for discovering and exploring movies and TV shows. 
+        Our platform offers a comprehensive database of films and series, complete with detailed information, ratings, and reviews (coming soon). 
+        Whether you're a casual viewer or a dedicated cinephile, geeDB provides all the tools you need to find your next favorite watch. 
+        Join our community and dive into the world of entertainment with geeDB.
+        </p>
+    </div>
+  )
+}
