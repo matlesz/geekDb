37c0fc1ce4b7125ed93e728f35c668acaa8711b1
matlesz <matlesz@gmail.com>
2024-08-03 19:17:47 +0100

    chore: Add DarkModeToggle component to header menu

---
+++ src/components/DarkModeToggle.jsx
@@ -0,0 +1,5 @@
+export default function DarkModeToggle() {
+  return (
+    <div>DarkModeToggle</div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from "./MenuItem";
 import { MdHomeFilled } from "react-icons/md";
 import { MdInfo } from "react-icons/md";
 import { RiAccountCircleFill } from "react-icons/ri";
+import DarkModeToggle from "./DarkModeToggle";
 
 export default function Header() {
   return (
@@ -12,14 +13,17 @@ export default function Header() {
             <MenuItem title="ABOUT" address="/about" Icon={MdInfo} />
         </div>
         
-        <div className="flex">
+        <div className="flex items-center space-x-5">
+            <DarkModeToggle />
+            <MenuItem title="Login" address="/login" Icon={RiAccountCircleFill} />
             <Link href="/">
               <h2 className="text-2xl font-bold text-green-950 bg-green-500 py-1 px-2 rounded-lg mr-1">geekDB</h2>
             </Link>
-            <MenuItem title="Login" address="/login" Icon={RiAccountCircleFill} />
         </div>
 
 
     </div>
   )
 }
+
+//1.21.08
