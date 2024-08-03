73d133e4bbe5ab05ca9981a6d6bf285f58fe1ffc
matlesz <matlesz@gmail.com>
2024-08-03 18:31:10 +0100

    chore: Update header menu item styling and add "About" button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
+import Link from "next/link";
 import MenuItem from "./MenuItem";
 import { MdHomeFilled } from "react-icons/md";
 import { MdInfo } from "react-icons/md";
 
 export default function Header() {
   return (
-    <div>
-        <div className="flex bg-green-950 text-green-500 p-4">
+    <div className="flex justify-between p-4 mx-2 max-w-6xl sm:mx-auto items-center py-6">
+        <div className="flex">
             <MenuItem title="HOME" address="/" Icon={MdHomeFilled} />
-            <MenuItem title="ABOUT" address="/" Icon={MdInfo} />
+            <MenuItem title="ABOUT" address="/about" Icon={MdInfo} />
         </div>
         
-        <div className="bg-green-950 text-green-500 p-4">
-
+        <div className="">
+            <Link href="/">
+              <h2 className="text-2xl font-bold text-green-950 bg-green-500 py-1 px-2 rounded-lg mr-1">geekDB</h2>
+            </Link>
         </div>
 
 
     </div>
   )
 }
-
-// 56:55
