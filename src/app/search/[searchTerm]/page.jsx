24dd68d57439e3a612329fa430a8cba9ede0c441
matlesz <matlesz@gmail.com>
2024-08-04 01:17:35 +0100

    feat: Add lazy loading for images in search results page

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,4 +1,5 @@
 import Results from "@/components/Results";
+import { Suspense } from 'react';
 
 export default async function SearchPage({params}) {
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&page=1&include_adult=false`);
@@ -11,11 +12,12 @@ export default async function SearchPage({params}) {
 
     const results = data.results;
     return (
-    <div>
-        {results && results.length === 0 &&
+    <Suspense fallback={<div>Loading...</div>}>
+        <div>
+            {results && results.length === 0 &&
             <h1 className="text-center pt-6">No results find</h1>}
-
-        {results && <Results results={results}/>}
-    </div>
+            {results && <Results results={results}/>}
+        </div>
+    </Suspense>
   )
 }
